9d4d971632afc0ff004216d43e136cc83b59802f
dazehinn <dazehinn@yahoo.fr>
2023-06-21 18:40:25 +0100

    Add dictionary & method to decode letter by letter

--- decoder.rb
+++ decoder.rb
@@ -1,3 +1,30 @@
+# Method that provides the morse table
+def morse_dictionary(morse_entry)
+  morse_dictionary = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+
+  morse_dictionary[morse_entry] || ''
+end
+
+# Decode letter by letter and arrange the words
+def word_decode(word_code)
+  encripted_char = word_code.split
+  decoded_word = ''
+
+  encripted_char.each do |char|
+    decoded_letter = morse_dictionary(char)
+    decoded_word += decoded_letter
+  end
+  decoded_word
+end
+
+
 # Decode word by word and arrange the phrase
 def phrase_decode(decoded_message)
   coded_word = decoded_message.split('   ')
@@ -29,3 +56,4 @@ puts decoded_morse_code3
 morse_code4 = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 decoded_morse_code4 = phrase_decode(morse_code4)
 puts decoded_morse_code4
+
